b9876ae6ed13fb0eb57f1bc467521e29cafd8f3b
Tom Flitterman <tom.flitterman@hotmail.fr>
2018-05-24 14:20:01 +0200

    Update index

--- public/index.php
+++ public/index.php
@@ -1,4 +1,18 @@
 <?php
 
-var_dump($_SERVER['REQUEST_URI']);
+$url = explode("/", $_SERVER['REQUEST_URI']);
 
+$page = $url[1];
+$pages = [
+    '',
+    'home',
+    'search',
+    'offers'
+];
+
+var_dump($url[1]);
+
+switch ($page) {
+    case '' :
+        include('../src/home.php');
+}
