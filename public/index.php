84f5c0c83619cbaefa557d9b24b91d248f2b92d5
Tom Flitterman <tom.flitterman@hotmail.fr>
2018-05-24 14:28:01 +0200

    Update index

--- public/index.php
+++ public/index.php
@@ -3,16 +3,19 @@
 $url = explode("/", $_SERVER['REQUEST_URI']);
 
 $page = $url[1];
-$pages = [
-    '',
-    'home',
-    'search',
-    'offers'
-];
 
-var_dump($url[1]);
+function getPage($p) {
 
-switch ($page) {
-    case '' :
-        include('../src/home.php');
+    if (empty($p)) {
+        return 'home.php';
+    }
+    elseif ($p == 'search') {
+        return 'search.php';
+    }
+    else {
+        return '404.html';
+    }
+
 }
+
+include('../src/' . getPage($page));
