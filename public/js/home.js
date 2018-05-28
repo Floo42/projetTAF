9137794da51fae6357463bab046619a162730e87
Anthony01640 <Anthony.dubuis@ynov.com>
2018-05-28 15:16:55 +0200

    js

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-});
+    function specialiteinfo() {
+        var mavariable = document.getElementById("them").value;
+        var spei = document.getElementById("info").value;
+        var speinfo = document.getElementById("info");
+        if (mavariable == spei){
+            speinfo.style.display = 'block';
+        }else{
+            console.log(mavariable);
+        }
+    }
+    var speinfo = document.getElementById("info");
+    speinfo.addEventListener("click",specialiteinfo);
+
+})
