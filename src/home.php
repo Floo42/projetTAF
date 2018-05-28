3f9c4d0a530476237b4144a5e965e4e53a442d02
Tom Flitterman <tom.flitterman@hotmail.fr>
2018-05-28 14:10:16 +0200

    Added search functionality

--- src/home.php
+++ src/home.php
@@ -1,4 +1,5 @@
-<html lang = fr>
+<!DOCTYPE html>
+<html>
 <head>
     <title>Ynov Lyon</title>
     <meta charset="UTF-8">
@@ -7,28 +8,40 @@
 </head>
 
     <body>
-    <h1>Trouvez des stagiaires et des alternants</h1>
+        <h1>Trouvez des stagiaires et des alternants</h1>
+
         <form action="<?php echo $domain ?>/search" method="POST" class="form">
-            <select name="thematique" class="thematique" id="them">
+
+            <select name="course" class="thematique" id="them">
                 <option value="default" selected>Thématique</option>
                 <option value="informatique" id="info">Informatique</option>
                 <option value="business">Business</option>
             </select>
-            <select name="contrat" class="contrat">
-        <option value="default" selected>Type de contrat</option>
-        <option value="stage">Stage</option>
-        <option value="alternance">Alternance</option>
-        </select>
-            <select name="specialite" class="specialite" id="speinfo">
+
+            <select name="contract" class="contrat">
+                <option value="default" selected>Type de contrat</option>
+                <option value="stage">Stage</option>
+                <option value="alternance">Alternance</option>
+            </select>
+
+            <select name="speciality" class="specialite" id="speinfo">
                 <option value="default" selected>Spécialité</option>
-                <option value="stage">IT</option>
-                <option value="alternance">Réseaux</option>
+                <option value="IT">IT</option>
+                <option value="reseaux">Réseaux</option>
+                <option value="SEO">SEO</option>
             </select>
-            <select name="nivetude" class="nivetude">
+
+            <select name="educationLevel" class="nivetude">
                 <option value="default" selected>Niveau d'étude</option>
-                <option value="bun">B1</option>
-                <option value="bdeux">B2</option>
+                <option value="1">B1</option>
+                <option value="2">B2</option>
+                <option value="3">B3</option>
+                <option value="4">M1</option>
+                <option value="5">M2</option>
             </select>
-</form>
-</body>
+
+            <input type="submit" value="Chercher">
+
+        </form>
+    </body>
 </html>
