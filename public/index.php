b499e82915e25c19c8e5ebe89f64afa46242a445
Tom Flitterman <tom.flitterman@hotmail.fr>
2018-05-28 14:36:12 +0200

    Updated database sql file

--- public/index.php
+++ public/index.php
@@ -1,5 +1,34 @@
 <?php
 
+function convertToUrl($string) {
+    if (!isset($string) or empty($string)) return false;
+    $string = mb_strtolower($string);
+    $string = str_replace(
+        array(
+            'à', 'â', 'ä', 'á', 'ã', 'å',
+            'î', 'ï', 'ì', 'í',
+            'ô', 'ö', 'ò', 'ó', 'õ', 'ø',
+            'ù', 'û', 'ü', 'ú',
+            'é', 'è', 'ê', 'ë',
+            'ç', 'ÿ', 'ñ',
+        ),
+        array(
+            'a', 'a', 'a', 'a', 'a', 'a',
+            'i', 'i', 'i', 'i',
+            'o', 'o', 'o', 'o', 'o', 'o',
+            'u', 'u', 'u', 'u',
+            'e', 'e', 'e', 'e',
+            'c', 'y', 'n',
+        ),
+        $string);
+    $string = str_replace(' ', '~', $string);
+    $string = filter_var($string, FILTER_SANITIZE_EMAIL);
+    $forbidden = array('!', '#', '$', '%', '&', '*', '+', '-', '/', '=', '?', '^', '_', '`', '{', '|', '}', '~', '@', '.', '[', ']', '\'', ' ');
+    $string = str_replace($forbidden, '-', $string);
+    $string = preg_replace('#-+#', '-', $string);
+    return trim($string, '-');
+}
+
 $jsonContent = file_get_contents('../config.json');
 $json = $json = json_decode($jsonContent, true);
 
