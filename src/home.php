3f34da41e8c743e1ff9fb28f90ba162521b73ec3
Anthony01640 <Anthony.dubuis@ynov.com>
2018-05-28 14:13:02 +0200

    le weekend c'est tendu

--- src/home.php
+++ src/home.php
@@ -7,12 +7,14 @@
 </head>
 
     <body>
-    <h1>Trouvez des stagiaires et des alternants</h1>
+    <h1>Trouvez des stagiaires<br> et des alternants</h1>
         <form action="<?php echo $domain ?>/search" method="POST" class="form">
             <select name="thematique" class="thematique" id="them">
                 <option value="default" selected>Thématique</option>
                 <option value="informatique" id="info">Informatique</option>
                 <option value="business">Business</option>
+                <option value="arts">Arts</option>
+                <option value="gamed">Game design</option>
             </select>
             <select name="contrat" class="contrat">
         <option value="default" selected>Type de contrat</option>
@@ -21,13 +23,18 @@
         </select>
             <select name="specialite" class="specialite" id="speinfo">
                 <option value="default" selected>Spécialité</option>
-                <option value="stage">IT</option>
-                <option value="alternance">Réseaux</option>
+                <option value="it">IT</option>
+                <option value="merde">Réseaux</option>
             </select>
-            <select name="nivetude" class="nivetude">
+
+
+            <select> name="nivetude" class="nivetude">
                 <option value="default" selected>Niveau d'étude</option>
-                <option value="bun">B1</option>
-                <option value="bdeux">B2</option>
+                <option value="biun">B1</option>
+                <option value="bideux">B2</option>
+                <option value="bitrois">B3</option>
+                <option value="miun">M1</option>
+                <option value="mideux">M2</option>
             </select>
 </form>
 </body>
