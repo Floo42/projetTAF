83a9ccf0a0c7015af217e1e0fc507ce48c79c020
Anthony01640 <Anthony.dubuis@ynov.com>
2018-05-30 16:25:02 +0200

    home css fin

--- public/js/home.js
+++ public/js/home.js
@@ -10,31 +10,37 @@ document.addEventListener("DOMContentLoaded", function () {
         var spemarket = document.getElementById("spemarket");
         var speg = document.getElementById("gamed").value;
         var spegamed = document.getElementById("spegamed");
+        var disable = document.getElementById("disable");
         if (mavariable == spei){
             speinfo.style.display = "block";
             spemarket.style.display = "none";
             speart.style.display = "none";
             spegamed.style.display = "none";
+            disable.style.display = "none";
         }else if(mavariable == spem){
             speinfo.style.display = "none";
             spemarket.style.display = "block";
             speart.style.display = "none";
             spegamed.style.display = "none";
+            disable.style.display = "none";
         }else if(mavariable == spea){
             speinfo.style.display = "none";
             spemarket.style.display = "none";
             speart.style.display = "block";
             spegamed.style.display = "none";
+            disable.style.display="none";
         }else if (mavariable == speg){
             speinfo.style.display = "none";
             spemarket.style.display = "none";
             speart.style.display = "none";
             spegamed.style.display = "block";
+            disable.style.display="none";
         }else {
             speinfo.style.display = "none";
             spemarket.style.display = "none";
             speart.style.display = "none";
             spegamed.style.display = "none";
+            disable.style.display="block";
         }
     }
     var maariable = document.getElementById("them");
